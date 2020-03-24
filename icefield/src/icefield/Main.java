d34e22d2dd70297ba57350d0c8b5ebe445b4068d
Bendegúz Kálmán <kalmanbendeguz@gmail.com>
2020-03-24 20:53:27 +0100

    df

--- icefield/src/icefield/Main.java
+++ icefield/src/icefield/Main.java
@@ -2,6 +2,6 @@ package icefield;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("CEREAL KILLERS");
+		System.out.println("CEREAL KILLERS sdfsfsdfdsgdf");
 	}
 }
