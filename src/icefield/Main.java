a1a689b421e1b33cc1e8b36fe1c73795ed39a995
jcsenge <58983729+jcsenge@users.noreply.github.com>
2020-03-24 21:12:09 +0100

    csenge

--- src/icefield/Main.java
+++ src/icefield/Main.java
@@ -2,6 +2,6 @@ package icefield;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("CEREAL KILLERS sdfsfsdfdsgdf");
+		System.out.println("CEREAL KILLERS from Csenge");
 	}
 }
