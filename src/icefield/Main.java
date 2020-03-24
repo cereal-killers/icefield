6981263e0c39de3f3bf67edc53c211784bf87eb5
Puskás Levente <frtjetson@gmail.com>
2020-03-24 22:16:50 +0200

    change

--- src/icefield/Main.java
+++ src/icefield/Main.java
@@ -3,5 +3,6 @@ package icefield;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("CEREAL KILLERS from Csenge");
+		System.out.println("CEREAL KILLERS from Levi");
 	}
 }
