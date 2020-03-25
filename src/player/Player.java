095a47b82bdbd5e756b87ab7a9a0360a8a472330
Puskás Levente <frtjetson@gmail.com>
2020-03-25 10:08:43 +0200

    kiegészítés

--- src/player/Player.java
+++ src/player/Player.java
@@ -1,22 +1,36 @@
 package player;
 
+import java.util.ArrayList;
+
 public class Player {
 	protected int health;
 	protected int maxHealth;
 	protected int energy;
 	protected boolean wears_suit;
-	
+	protected ArrayList<Item> items;
+	protected Field currentField;
 	
 	public Player() {
 		this.energy = 4;
+		ArrayList<Item> items = new ArrayList<Item>(5);
+		
 	}
 	
 	public void PickItemUp() {
+		Item item = currentField.GetItem();
+		try {
+			currentField.PopItem();
+			this.AddItem(item);
+		}catch(Exception e) {
+			System.out.println("Nincs több hely az eszköztárban");
+		}
 		
 	}
 	
 	public void DropItem(Item item) {
-		
+		try {
+			items.remove(item);
+		}
 	}
 	
 	public void EndTurn() {
