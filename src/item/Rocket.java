2c2e4920e502dd0c5fe2bda074c8dd5022a57675
Bendegúz Kálmán <kalmanbendeguz@gmail.com>
2020-03-25 22:36:10 +0100

    Rocket

---
+++ src/item/Rocket.java
@@ -0,0 +1,13 @@
+package item;
+
+public class Rocket {
+	private boolean isOnOneField;
+	
+	public void Assemble() {
+		System.out.println("Assemble()");
+	}
+	
+	public void Fire() {
+		System.out.println("Fire()");
+	}
+}
