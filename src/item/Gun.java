c0e69c3dbb87551188c63b7bf6dcaeeff6558386
Bendegúz Kálmán <kalmanbendeguz@gmail.com>
2020-03-27 16:12:56 +0100

    rocket constructor

--- src/item/Gun.java
+++ src/item/Gun.java
@@ -3,7 +3,7 @@ package item;
 import player.Player;
 
 public class Gun extends Item {
-
+	
 	@Override
 	public void Use(Player player) {
 		System.out.println("gun cannot be used individually");
--- src/item/Rocket.java
+++ src/item/Rocket.java
@@ -3,9 +3,21 @@ package item;
 import icefield.Controller;
 
 public class Rocket {
-	private boolean isOnOneField;
+	private Controller controller;
+	private Gun gun;
+	private Flare flare;
+	private Charge charge;
 
-    public Rocket(Controller controller) {
+    public Rocket(Controller controller, Gun gun, Flare flare, Charge charge) {
+    	this.controller = controller;
+    	this.gun = gun;
+    	this.flare = flare;
+    	this.charge = charge;
+    }
+    
+    private boolean isOnOneField() {
+    	//todo
+    	return false;
     }
 
     public void Assemble() {
