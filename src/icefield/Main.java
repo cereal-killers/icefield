0959dab23500443d52d05f81a045fdaf7f6f31f0
Bendegúz Kálmán <kalmanbendeguz@gmail.com>
2020-03-27 01:14:48 +0100

    for safety

--- src/icefield/Main.java
+++
@@ -1,9 +0,0 @@
-package icefield;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("CEREAL KILLERS from Csenge");
-		System.out.println("CEREAL KILLERS from Levi");
-		System.out.println("CEREAL KILLERS from Sári");
-	}
-}
