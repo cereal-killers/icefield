3ed126ef62e01f7558145ccb91a26b15abfff858
Bendegúz Kálmán <kalmanbendeguz@gmail.com>
2020-04-25 17:23:59 +0200

    tent

--- src/item/Tent.java
+++ src/item/Tent.java
@@ -9,7 +9,14 @@ public class Tent extends Item implements java.io.Serializable {
 	public void Use(Player player) {
 		
 		System.out.println("Use(Player player)");
-		player.getCurrentField()player.
+		if(!player.getCurrentField().getHasTent()) {
+			player.getCurrentField().setHasTent(true);
+			player.RemoveItem(this);
+			System.out.println("Tent set");
+		} else {
+			System.out.println("Field already has tent");
+		}
+		
 	}
 	
 	@Override
