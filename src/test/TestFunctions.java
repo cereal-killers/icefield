9b2c3814ba75753c073f960c5f7f2ee9de8daeb5
esocsepp <kuzsnersara@gmail.com>
2020-04-25 17:38:01 +0200

    Spawn Functions Declaration

---
+++ src/test/TestFunctions.java
@@ -0,0 +1,42 @@
+package test;
+
+import field.Field;
+import item.Item;
+
+public class TestFunctions
+{
+    public void SpawnEskimo(Field field, Item item)     //Eszkimo lehelyezese adott mezore
+    {
+
+    }
+
+    public void SpawnScientist(Field field, Item item)  //Kutato lehelyezese adott mezore
+    {
+
+    }
+
+    public void SpawnPolarBear(Field field)             //Jegesmaci lehelyezese adott mezore
+    {
+
+    }
+
+    public void SpawnTent(Field field)                  //Sator lehelyezese adott mezore
+    {
+
+    }
+
+    public void SpawnIgloo(Field field)                  //Iglu lehelyezese adott mezore
+    {
+
+    }
+
+    public void SpawnItem(Field field, Item item)        //Sator lehelyezese adott mezore
+    {
+
+    }
+
+    public void SpawnSnowStorm(Field field)              //Iglu lehelyezese adott mezore
+    {
+
+    }
+}
