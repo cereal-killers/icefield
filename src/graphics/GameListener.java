aa1647b8618a5e586c9ed0d1c6fc62cef088a028
Bendegúz Kálmán <kalmanbendeguz@gmail.com>
2020-05-15 17:38:22 +0200

    gamelistener: még a togglemusic hiányzik

--- src/graphics/GameListener.java
+++ src/graphics/GameListener.java
@@ -9,47 +9,58 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+
+import field.Field;
 import graphics.Container;
 import icefield.Controller;
 import menu.Menu;
 
-public class GameListener implements ActionListener, KeyListener {
+public class GameListener implements ActionListener, KeyListener, MouseListener {
 
 	private Container container;
+	private Controller controller;
+	private PrintStream stdout;
+	private InputStream stdin;
 	
-	public GameListener(Container _container) {
+	public GameListener(Container _container, Controller _controller) {
 		this.container = _container;
+		stdout = System.out;
+		stdin = System.in;
 	}
+	
 	@Override
-	public void actionPerformed(ActionEvent e) { // még csak a modellen végzett változtatások vannak, a viewen nem
-		String[] cmd_in = e.getActionCommand().split("\\s+");
-		String cmd_out = "";
-		InputStream stdin = System.in;
-		System.setIn(stdin);
+	public void actionPerformed(ActionEvent e) { 
+		String[] cmd_from_view = e.getActionCommand().split("\\s+");
+		String cmd_to_model = "";
 		
-
-		switch(cmd_in[0]) {
+		switch(cmd_from_view[0]) {
 			case "newgame":{
-				cmd_out = "1";
+				cmd_to_model = "1";
 				container.navigate("choosemap");
 			} break;
 			case "options":{
-				cmd_out = "2";
+				cmd_to_model = "2";
 				container.navigate("options");
 			} break;
 			case "highscores":{
-				cmd_out = "3";
+				cmd_to_model = "3";
 				container.navigate("highscores");
 			} break;
 			case "exit":{
-				cmd_out = "4";
+				cmd_to_model = "4"; //talán az ablakkal is külön kell majd valamit kezdeni?
 			} break;
 			case "submit":{
-				/*String newName = optionsPanel.GetNameTextField().getText();
-				cmd_out = "1\r\n" + newName;
-				container.savePlayerName();*/
+				String newName = cmd_from_view[1];
+				cmd_to_model = "1" + System.lineSeparator() + newName;
 			} break;
 			case "togglemusic":{
 				/*boolean music = menu.getOptions().getMusic();
@@ -61,63 +72,110 @@ public class GameListener implements ActionListener, KeyListener {
 				container.toggleMusic();*/
 			} break;
 			case "remove_snow":{
-				cmd_out = "remove snow";
+				cmd_to_model = "remove snow";
 			}break;
 			case "build_igloo":{
-				cmd_out = "build igloo";
+				cmd_to_model = "build igloo";
 			}break;
 			case "pick_up_item":{
-				cmd_out = "pick up item";
+				cmd_to_model = "pick up item";
 			}break;
 			case "menu":{
-				cmd_out = "menu";
+				cmd_to_model = "menu";
 			}break;
 			case "end_turn": {
-				cmd_out = "end turn";
+				cmd_to_model = "end turn";
 			}break;
 			case "nagy": {
-				cmd_out = "nagypalya";
+				cmd_to_model = "nagypalya";
 				container.navigate("nagy");
 			}break;
 			case "foci": {
-				cmd_out = "focipalya";
+				cmd_to_model = "focipalya";
 				container.navigate("foci");
 			}break;
 			case "teszt": {
-				cmd_out = "tesztpalya";
+				cmd_to_model = "tesztpalya";
 				container.navigate("teszt");
 			}break;
 			case "move": {
-				cmd_out = "tesztpalya";
-				container.navigate("teszt");
+				int globalIndex = Integer.parseInt(cmd_from_view[1]); // 0-tól indul
+				Field moveTo = controller.getFields().get(globalIndex); //0-tól indul
+				int direction = controller.getCurrentPlayer().getCurrentField().getNeighbors().indexOf(moveTo); // 0-tól indul
+				int dirToModel = direction + 1; // a move parancsot 1-től kell indexelni
+				cmd_to_model = "move " + dirToModel; 
 			}break;
 			case "inspect": {
-				cmd_out = "tesztpalya";
+				cmd_to_model = "tesztpalya";
 				container.navigate("teszt");
 			}break;
 			case "use": {
-				cmd_out = "tesztpalya";
-				container.navigate("teszt");
+				String item = cmd_from_view[1];
+				cmd_to_model = "use "+ item;
 			}break;
 			default: {
 				
 			} break;
 		}
-		System.setIn(new ByteArrayInputStream(cmd_out.getBytes()));
-		container.repaint();
+		sendCommandToModel(cmd_to_model);
+		container.repaint(); //biztos ez kell? nem implementáltad, magától meg nem kérdezi le a modellt
 	}
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		String cmd_out = "";
-		InputStream stdin = System.in;
-		System.setIn(stdin);
+		String cmd_to_model = "";
 		if(key == KeyEvent.VK_ESCAPE) {
-			cmd_out = "menu";
+			cmd_to_model = "menu";
 		}
-		System.setIn(new ByteArrayInputStream(cmd_out.getBytes()));
+		sendCommandToModel(cmd_to_model);
 		container.repaint();
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		JButton j = new JButton();
+		try {
+			j = (JButton) e.getComponent();
+		} catch(Exception ex) {}
+		
+		if(e.getButton() == MouseEvent.BUTTON3) {
+			String[] cmd_from_view = j.getActionCommand().split("\\s+"); // az meg lesz oldva hogy ha rossz helyre kattintasz akkor invalid command jön? mondjuk ""
+			String cmd_to_model = "";
+			
+			switch(controller.getCurrentPlayer().getName()) {
+				case "Eskimo":{
+					int globalIndex = Integer.parseInt(cmd_from_view[1]);
+					Field toInspect = controller.getFields().get(globalIndex);
+					int direction = controller.getCurrentPlayer().getCurrentField().getNeighbors().indexOf(toInspect);
+					int dirToModel = direction + 1;
+					cmd_to_model = "inspect " + dirToModel;
+				} break;
+				case "Scientist":{
+					cmd_to_model = "igloo";
+				} break;
+				default: break;
+			}
+			sendCommandToModel(cmd_to_model);
+			container.repaint();
+		}
+		
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	@Override
@@ -131,5 +189,28 @@ public class GameListener implements ActionListener, KeyListener {
 		// TODO Auto-generated method stub
 
 	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void sendCommandToModel(String cmd) {
+		cmd = cmd + System.lineSeparator();
+		System.setIn(new ByteArrayInputStream(cmd.getBytes()));
+		//Scanner scanner = new Scanner(System.in);
+		//System.out.println(scanner.nextLine());
+		//scanner.close();
+		
+		stdout.println(cmd); //debug célból
+	}
+	
+	/*private String getOutputFromModel() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream(); //olvas
+		PrintStream ps = new PrintStream(baos); //olvas
+		PrintStream stdout = System.out; //olvas
+		System.setOut(ps); //olvas
+		re
+	}*/
 
 }
